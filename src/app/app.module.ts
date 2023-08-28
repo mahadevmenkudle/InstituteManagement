297cfe58359fdfe1a1b4ef041bf510d71608ea41
krishna <krishnanalgire95@gmail.com>
2023-08-28 19:21:41 +0530

    1. creating datalist component 2. creating employeelist component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { DatalistComponent } from './datalist/datalist.component';
+import { EmployeelistComponent } from './employeelist/employeelist.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DatalistComponent,
+    EmployeelistComponent
   ],
   imports: [
     BrowserModule
